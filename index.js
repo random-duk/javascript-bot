976eee9d77b7eeaefb62b94f7fcd013912238b37
Ryan <RyanMcKeown82@Gmail.com>
2018-07-24 20:51:25 +0800

    clean up code
    - Proposed the idea of importing individual classes
    - Switched to node-fetch since snekfetch is deprecated 
    - Catching error if one occurs 
    - Closed `'` on last line of code since you forgot to

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-const discord = require('discord.js');
-const client = new discord.Client();
-const request = require('snekfetch');
+const { Client, RichEmbed } = require('discord.js');
+const client = new Client();
+const fetch = require('node-fetch');
 
 client.on('ready', () => {
-    console.log('Ducky logged in!');
-}
-    );
-
-client.on('message', msg => {
-    if (msg.content === '!duckjs') {
-        request.get('https://api.random-d.uk/random').then((result) => {
-            const embed = new discord.RichEmbed().setTitle('A duck in JS for you!').setAuthor(msg.author.username, msg.author.avatarURL).setImage(result.body.url).setFooter(result.body.message);
-            msg.channel.send({embed});
-        });
-        }
+  console.log('Ducky logged in!');
 });
 
-client.login('token)
+client.on('message', msg => {
+  if (msg.content === '!duck') {
+    fetch('https://api.random-d.uk/random').then((result) => {
+      const embed = new RichEmbed()
+      .setTitle('A duck in JS for you!')
+      .setAuthor(msg.author.username, msg.author.avatarURL)
+      .setImage(result.body.url)
+      .setFooter(result.body.message);
+            
+      msg.channel.send({ embed });
+    }).catch(err => console.error(err));
+  }
+});
+
+client.login('BOT_TOKEN');
